c447803ba15b8105ddd04bb61c5092701ed17331
Christian Toft <christian.toft1@gmail.com>
2015-12-14 13:18:52 +0100

    fix to temp_ambient and temp_object for c3_read_ble_24bit.m

--- c3_read_ble_24bit.m
+++ c3_read_ble_24bit.m
@@ -11,7 +11,7 @@
 % int16     acc_z;
 % uint16    temp_ambient_object; % temp_ambient and temp_object alternate in every second (modulus 2) batch
 % uint16    serial_ADS;
-% uint8     bat_status; % bat_level and bat_status, packed into bitmask
+% uint8     bat_level_and_status; % bat_level and bat_status, packed into bitmask
 % bit24     resp; % 24 bit, signed, respiration sample
 % int8      leadoff;
 % uint8     conf;       % bitmask indicating which (if any) of the RESP, ECG1, ECG2, ECG3 parts appear after Part 1. This configuration never changes within the current BLE file.
@@ -38,6 +38,7 @@ function [serialNumber, conf, serial_ADS, leadoff, acc, temp, resp, ecg, ecg_ser
             posForValidConf = ftell(fid);
             fprintf('valid conf assumed at file pos: %d bytes\n',posForValidConf);
             valid_conf = fread(fid, 1, '*uint8');
+            first_valid_serial = tempSerialNumber;
             break;
         end
         % Go to next position for a possible serial number.
@@ -57,7 +58,6 @@ function [serialNumber, conf, serial_ADS, leadoff, acc, temp, resp, ecg, ecg_ser
     ecg3Available = (conf_bin(end-3) == '1');
     % dec2hex for debugging purposes
     conf_hex = dec2hex(valid_conf);
-    fprintf('Conf DEC: %d     Conf BIN: %s    Conf HEX: %s\n',valid_conf,conf_bin,conf_hex);
 
     % set the file pointer to the start of the file
     frewind(fid);
@@ -77,7 +77,7 @@ function [serialNumber, conf, serial_ADS, leadoff, acc, temp, resp, ecg, ecg_ser
     fseek(fid, 12, 'bof'); % rewind
     serial_ADS = fread(fid, numBatches, '*uint16', batchSize-2);
     fseek(fid, 14, 'bof'); % rewind
-    bat_status = fread(fid, numBatches, '*uint8', batchSize-1);
+    bat_level_and_status = fread(fid, numBatches, '*uint8', batchSize-1);
     if respAvailable
         fseek(fid, 15, 'bof'); % rewind
         resp = fread(fid, numBatches, 'bit24', (batchSize*8)-24); % when using bitn, 'skip' argument must be specified in bits, not bytes
@@ -153,23 +153,20 @@ function [serialNumber, conf, serial_ADS, leadoff, acc, temp, resp, ecg, ecg_ser
     end
     fclose(fid);
     
+    % Split temp into ambient and object
+    % ambient when  (serial % 2 ==0) and object when (serial % 2 == 1) ambient
+    
     if debug
         % For debugging using bat_adc
-        disp('DEBUGGIING SET IN read_ble.m');
+        disp('DEBUGGIING SET IN read_ble_24bit.m');
         figure;plot(bat_adc);
         table(unique(bat_adc), histc(bat_adc(:),unique(bat_adc)), round(double(histc(bat_adc(:),unique(bat_adc))./double(length(bat_adc)))*100), 'VariableNames',{'bat_adc' 'Count' 'Percent'})
-        temp_object = temp_amb_obj(1:2:end);
-        temp_ambient = temp_amb_obj(2:2:end);
-    else
-        % Scaling temp to Celsius, and accel to g-force
-        % and splitting temp into temp_ambient and tenp_object
-        temp_amb_obj = (temp_amb_obj * 0.02) - 273.15;
-        temp_object = temp_amb_obj(1:2:end);
-        temp_ambient = temp_amb_obj(2:2:end);
-        acc_x = acc_x * 0.00006103515625;
-        acc_y = acc_y * 0.00006103515625;
-        acc_z = acc_z * 0.00006103515625;
     end
+    
+    % Scale accel to g-force
+    acc_x = acc_x * 0.00006103515625;
+    acc_y = acc_y * 0.00006103515625;
+    acc_z = acc_z * 0.00006103515625;
     
     % find indices of missed batches
     missedBatches = find(serialNumber == 0);
@@ -179,9 +176,12 @@ function [serialNumber, conf, serial_ADS, leadoff, acc, temp, resp, ecg, ecg_ser
         acc_x(missedBatches) = NaN;
         acc_y(missedBatches) = NaN;
         acc_z(missedBatches) = NaN;
-        temp_ambient(missedBatches) = NaN;
-        temp_object(missedBatches) = NaN;
-        % building index numbers for missed samples in resp and ecg (10 times as many as in accel and temp)
+        temp_amb_obj(missedBatches) = NaN;
+        % Setting invalid resp samples to NaN may cause trouble in subsequent filtering
+%       if ~isempty(resp)
+%           resp(missedBatches) = NaN;
+%       end
+        % building index numbers for missed samples in resp and ecg (6 times as many as in accel, temp, and resp)
         startOfMissed_ecg_resp = ((missedBatches-1)*6)+1;
         endOfMissed_ecg_resp = missedBatches*6;
         idx = coloncat(startOfMissed_ecg_resp, endOfMissed_ecg_resp);
@@ -194,20 +194,44 @@ function [serialNumber, conf, serial_ADS, leadoff, acc, temp, resp, ecg, ecg_ser
         if ~isempty(ecg3)
             ecg3(idx) = -32768; % NaN or -32768 for Comm error
         end
-        % Setting invalid resp samples to NaN may cause trouble in subsequent lowpass filtering
-%         if ~isempty(resp)
-%             resp(idx) = NaN;
-%         end
     end
+    
+    % Scaling temp to Celsius
+    temp_amb_obj = (temp_amb_obj * 0.02) - 273.15;    
+    % Splitting temp into temp_ambient and tenp_object.
+    % temp_ambient is stored in batches where mod(serialNumber,2) == 0.
+    batchesBeforeFirstValidSerial = (posForValidConf - 19) / batchSize;
+    serialOfFirstBatch = first_valid_serial - batchesBeforeFirstValidSerial;
+    if mod(serialOfFirstBatch,2) == 0
+        temp_ambient = temp_amb_obj(1:2:end);
+        temp_object = temp_amb_obj(2:2:end);
+    else
+        temp_ambient = temp_amb_obj(2:2:end);
+        temp_object = temp_amb_obj(1:2:end);
+    end
+    
     acc = [acc_y, -acc_x, acc_z]; % yes, acc_x = acc_y, and yes, acc_y = -acc_x
-    % if we have one less temp_ambient sample than temp_object (because of odd munber of batches)
+    % if we have one less temp_ambient or temp_object sample (because of
+    % odd munber of batches), extend with a copy of last sample.
     if (length(temp_object) - length(temp_ambient)) == 1
         % extend temp_ambient with one sample equal to the final sample
         temp_ambient(end+1) = temp_ambient(end);
+    elseif (length(temp_ambient) - length(temp_object)) == 1
+        % extend temp_object with one sample equal to the final sample
+        temp_object(end+1) = temp_object(end);
     end
+    
     temp = [temp_ambient, temp_object];
     ecg = [ecg1, ecg2, ecg3];
     ecg_serials = [ecg1_serial, ecg2_serial, ecg3_serial];
+    
+    % info about recording start time and duration, output to console
+    [~,ble_filename_wo_extension,ble_extension] = fileparts(ble_fullpath);
+    recordingStartTime = datetime(hex2dec(ble_filename_wo_extension), 'ConvertFrom', 'posixtime', 'TimeZone', 'local');%'Europe/Zurich'
+    LengthOfRecondingInSeconds = length(serialNumber) * 0.024;
+    [h,m,s] = hms(seconds(LengthOfRecondingInSeconds));    
+    fprintf('File name: %s    Start: %s    Duration: %02d:%02d:%02.0f (hours:mins:secs)\n',[ble_filename_wo_extension,ble_extension], datestr(recordingStartTime), h, m, s);
+    fprintf('Conf DEC: %d     Conf BIN: %s    Conf HEX: %s\n',valid_conf,conf_bin,conf_hex);
 end
 
 function idx = coloncat(start, stop)
